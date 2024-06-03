b47f3624ab2ec39559390723ada487caf4b66776
Tait Hoyem <tait@tait.tech>
2024-06-03 11:29:25 -0600

    Fix tests

--- fry_normalize/src/lib.rs
+++ fry_normalize/src/lib.rs
@@ -152,7 +152,7 @@ mod tests {
 	#[test]
 	fn regex_loads_successfully() {
 		let map = regex_map().unwrap();
-		assert!(!map.len().is_empty(), "The map must contain at least one item");
+		assert!(!map.is_empty(), "The map must contain at least one item");
 	}
 	#[test]
 	fn test_number_normalization() {
