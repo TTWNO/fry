6f854186f68ef3e087eb10b59a594137a2e80b84
Tait Hoyem <tait@tait.tech>
2024-06-03 11:21:03 -0600

    Revert "Use serde"
    This reverts commit 8df223a057be01215044bbf0749ef3cc4b0fc5f7.

--- fry_normalize/src/lib.rs
+++ fry_normalize/src/lib.rs
@@ -2,7 +2,6 @@
 
 extern crate alloc;
 
-use serde::{Serialize, Deserialize};
 use enum_dispatch::enum_dispatch;
 use num2words::{
 	Num2Words,
@@ -73,7 +72,7 @@ macro_rules! auto_into_enum {
 	}
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug)]
 pub enum Error {
 	Regex(regex::Error),
 	Num(Num2Err),
